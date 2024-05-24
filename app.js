c114da5a91bc80357c1f20e8cfebf15ae867e1cd
quan1997ap <quan1997ap@gmail.com>
2024-05-24 11:05:53 +0700

    Init

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+let users = [{ name: "Alice" }, { name: "Bob" }];
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/update-user", (req, res) => {
+  console.log(req.body);
+  res.json(users);
+});
