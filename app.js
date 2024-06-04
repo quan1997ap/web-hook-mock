5e03f77270228f7104c6916e0fd31f9c9d5b6fa6
quan1997ap <quan1997ap@gmail.com>
2024-06-04 09:21:26 +0700

    Update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -21,3 +22,6 @@ app.post("/update-user", (req, res) => {
   console.log(req.body);
   res.json(users);
 });
+
+
+// http://localhost:5000/conponents/tempate-proxy-call-api.yaml.yaml
